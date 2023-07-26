8b5f713592fb2b57afa8ad4651b9bb7cba28c41e
williamschmidt-git <9williamschmidt@gmail.com>
2023-07-25 21:09:42 -0300

    handle change logic + css

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -8,10 +8,48 @@ interface Inputs {
   title: string;
 }
 
+type Form = {
+  Bem_vindo: string;
+  Estado_civil: string;
+  Grau_de_Instrução: string;
+  Nome_da_mãe: string;
+  Nome_da_tia: string;
+  Nome_do_gato: string;
+  Nome_do_pai: string;
+  Número_do_RG: string;
+  Registro_profissional: string;
+  Data_de_nascimento: string;
+  Número_da_carteira_de_trabalho: string;
+  Número_da_CNH: string;
+  PIS_ou_NIS: string;
+  Telefone: string;
+  Dados_bancários: string;
+  Endereço_residencial: string;
+}
+
+const defaultForm: Form = {
+  Bem_vindo: '',
+  Estado_civil: '',
+  Grau_de_Instrução: '',
+  Nome_da_mãe: '',
+  Nome_da_tia: '',
+  Nome_do_gato: '',
+  Nome_do_pai: '',
+  Número_do_RG: '',
+  Registro_profissional: '',
+  Data_de_nascimento: '',
+  Número_da_carteira_de_trabalho: '',
+  Número_da_CNH: '',
+  PIS_ou_NIS: '',
+  Telefone: '',
+  Dados_bancários: '',
+  Endereço_residencial: '',
+};
+
 export function Form () {
   const [inputs, setInputs] = useState<Inputs[]>([]);
-  // const [renderInputs, setRenderInputs] = useState<Inputs[]>([]);
-  // const [formState, setFormState] = useState([]);
+  const [secondTable, setSecondTable] = useState<Inputs[]>([]);
+  const [formState, setFormState] = useState<Form>(defaultForm);
 
   useEffect(() => {
     async function fetchData() {
@@ -21,9 +59,9 @@ export function Form () {
 
       sortArray(json);
 
-      const [separatedArrays] = separateArray(json);
+      switchBlankSpaceToUnderscoreOfTitleAttribute(json);
 
-      setInputs(separatedArrays);
+      setInputs(json);
     }
 
     fetchData();
@@ -50,35 +88,43 @@ export function Form () {
     });
   }
 
-  function separateArray (array: Inputs[]) {
-    console.log(array);
-    const half = Math.ceil(array.length / 2);
+  function switchBlankSpaceToUnderscoreOfTitleAttribute(array: Inputs[]) {
+    array.forEach((e) => {
+      e.title = e.title.replace(/\s+/g,'_');
+    });
+  }
 
-    const firstHalf = array.slice(0, half);
-    const secondHalf = array.slice(half);
-
-    return [firstHalf, secondHalf];
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const {name, value} = event.target;
+    setFormState(prevState => ({
+      ...prevState, [name]: value
+    }));
   }
 
   return(
     <form className='form'>
       {
-        inputs.length > 0 ? (<>
-          {
-            inputs.map((e) => {
-              return (
-                <div key={e.id}>
-                  <label htmlFor={e.title} >
-                    {e.title}
-                    <input type='text' className='input'/>
-                  </label>
-                </div>
-              );
-            })
-          }
-          <button>Enviar</button>
-        </>) : null
+        inputs.length > 0 ? (
+          <div className='wrapper'>
+            {
+              inputs.map((e) => {
+                return (
+                  <div key={e.id} className='input-wrapper'>
+                    <label htmlFor={e.title} className='label'>
+                      {e.title}
+                    </label>
+                    <input type='text' className='input' name={e.title} 
+                      onChange={handleChange}/>
+                  </div>
+                );
+              })
+            }
+          </div>
+        ) : null
       }
+
+      <button>Enviar</button>
+
     </form>
   );
 }
